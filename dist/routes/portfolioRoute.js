fa7199250c7aed6a761315e296d252d94711c951
Tanabodee Boonrak <tanabodee1409.dev@gmail.com>
2023-12-13 21:05:57 +0700

    first commit

---
+++ dist/routes/portfolioRoute.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const { createEducation, getEducation, updateEducation, deleteEducation } = require('../controllers/educationController');
+const { createExperience, getExperience, updateExperience, deleteExperience } = require('../controllers/experienceController');
+const { getExportPortfolio } = require('../controllers/exportController');
+const { requireAuth } = require('../middlewares/authMiddleware');
+const router = (0, express_1.Router)();
+//POST Methods */
+router.post('/createEducation', requireAuth, createEducation);
+router.post('/createExperience', requireAuth, createExperience);
+//GET Methods /
+router.get('/getEducation', requireAuth, getEducation);
+router.get('/getExperience', requireAuth, getExperience);
+router.get('/getExportPortfolio', requireAuth, getExportPortfolio);
+//**PUT Methods
+router.put('/updateEducation', requireAuth, updateEducation);
+router.put('/updateExperience', requireAuth, updateExperience);
+//*DELETE Methods/
+router.delete('/deleteEducation', requireAuth, deleteEducation);
+router.delete('/deleteExperience', requireAuth, deleteExperience);
+module.exports = router;
